aa12b30802dba32a6e1571c7ecd81839dec1635d
Victor Gomesm <victorgbpk@gmail.com>
2023-11-27 09:59:24 -0300

    Projeto Finalizado

---
+++ script.bat
@@ -0,0 +1,17 @@
+echo "Criando as imagens..."
+
+docker build -t victorfg21/projeto-backend:1.0 backend/.
+docker build -t victorfg21/projeto-database:1.0 database/.
+
+echo "Realizando push das imagens..."
+
+docker push victorfg21/projeto-backend:1.0
+docker push victorfg21/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
